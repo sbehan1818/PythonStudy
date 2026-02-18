cd1df21bfae98e988b81006f7c18627cee0f68d1
Scott Behan <your@email.com>
2026-02-18 20:41:34 +0000

    commit

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # Asks user for name
-name = input ("Whats your name? ").strip().title()
+name = input ("Please enter first and last name? ").strip().title()
 
 # removes whitspace from the variable so only text is there using a function but only removes the left of and right of the inputed text
 #name = name.strip()
@@ -13,6 +13,9 @@ name = input ("Whats your name? ").strip().title()
 # chain the functions on the string
 #name = name.strip().title()
 
+# split usersname using the split function will split into variables in order in string first word into first variable and so on
+first, last = name.split()
+
 # Says hello to user
 print ("Hello,",name)
 
@@ -25,3 +28,7 @@ print ("Hello,",name, sep="Test")
 
 # tells python speical string and formats differently so pulls out the variable between {}
 print (f"Hello, {name}")
+
+# print what there first name is and there last
+print (f"Your first name is, {first}")
+print (f"Your last name is, {last}")
